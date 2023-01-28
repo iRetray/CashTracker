a758d241f2b4633d7b82a47173cd704d39911d66
Julian Camilo Cruz Sanchez <prv_julian.cruz@dale.com.co>
2023-01-28 15:54:22 -0500

    Initial commit

---
+++ CashTransfer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const CashTransfer = (): JSX.Element => (
+  <SafeAreaView>
+    <Text>Cash transfer, ready to go?</Text>
+  </SafeAreaView>
+);
+
+export default CashTransfer;
---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+
+import { name as appName } from './app.json';
+import CashTransfer from './CashTransfer';
+
+AppRegistry.registerComponent(appName, () => CashTransfer);
