34ff9b817efc683c85a5fe10341778e905f2e291
Julian Camilo Cruz Sanchez <julian20141016@gmail.com>
2023-01-28 16:09:02 -0500

    Installed Typescript and Native Base

--- CashTransfer.tsx
+++ CashTransfer.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 
+import { NativeBaseProvider } from 'native-base';
+
+interface NativeBaseWrapperProps {
+  children: JSX.Element;
+}
+
+const NativeBaseWrapper = ({
+  children,
+}: NativeBaseWrapperProps): JSX.Element => (
+  <NativeBaseProvider>{children}</NativeBaseProvider>
+);
+
 const CashTransfer = (): JSX.Element => (
-  <SafeAreaView>
-    <Text>Cash transfer, ready to go?</Text>
-  </SafeAreaView>
+  <NativeBaseWrapper>
+    <SafeAreaView>
+      <Text>Cash transfer, ready to go?</Text>
+    </SafeAreaView>
+  </NativeBaseWrapper>
 );
 
 export default CashTransfer;
